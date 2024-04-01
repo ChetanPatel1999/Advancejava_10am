c7061e7a2a152785fbdf93039066f117b706ac9b
Chetan Patel <155350722+ChetanPatel1999@users.noreply.github.com>
2024-04-01 11:11:01 +0530

    01/04

---
+++ JDBCBYCHETAN/src/Dataretrieve.java
@@ -0,0 +1,24 @@
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
+public class Dataretrieve {
+    public static void main(String[] args) throws Exception {
+        Connection con=Utility.connect();
+        Statement st=con.createStatement();
+        String sql="select *from student ";
+        ResultSet rs=st.executeQuery(sql);
+        while(rs.next())
+        {
+        String s1=rs.getString(1);
+        String s2=rs.getString(2);
+        String s3=rs.getString(3);
+        String s4=rs.getString(4);
+        System.out.println(s1+ " "+s2+ " "+s3+ " "+s4);
+        }
+        
+        
+    }
+}
